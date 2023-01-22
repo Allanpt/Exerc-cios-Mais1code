e8a6f0063f3c4f3ef69febbece896ba0661051f1
Allanpt <jatallantavares@gmail.com>
2023-01-22 10:09:41 -0300

    Array em ordem alfabética

---
+++ Exercícios com Arrays/ordemAlfabetica.js
@@ -0,0 +1,5 @@
+const ordemAlfabetica = (arrayComStrings) => {
+    const arrayOrganizada = arrayComStrings.sort()
+    return arrayOrganizada
+} 
+console.log(ordemAlfabetica([])) // Digite um Array de strings dentro da Função
