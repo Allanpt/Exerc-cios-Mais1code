46c06ae4d2cdd26cf6d2fce6be2151da28629ea7
Allanpt <99320857+Allanpt@users.noreply.github.com>
2023-01-22 10:01:24 -0300

    Delete menorNumero.js

--- menorNumero.js
+++
@@ -1,5 +0,0 @@
-const menorNumero = (arrayComNumeros) => {
-    const arrayOrganizada = arrayComNumeros.sort()
-    return `O menor número do Array é o ${arrayOrganizada[0]}`
-} 
-console.log(menorNumero()) // Digite um Array dentro da Função
