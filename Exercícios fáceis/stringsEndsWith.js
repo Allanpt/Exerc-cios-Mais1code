204824e7308c137570bd63d58c39bc91560b6dab
Allanpt <jatallantavares@gmail.com>
2023-01-23 19:58:51 -0300

    Strings Ends With...

--- Exercícios fáceis/stringsEndsWith.js
+++ Exercícios fáceis/stringsEndsWith.js
@@ -2,6 +2,17 @@
     Crie uma função que retorne TRUE caso o primeiro parametro(String) termine com o segundo parametro(String). Senão, retorna FALSE
 */
 
-
-
+const stringsEndsWith = (palavra1, palavra2) => {
+    if(typeof(palavra1,palavra2) == "string") {
+        if(palavra1.substring(palavra1.length - palavra2.length) == palavra2) {
+            return true
+        } else {
+            return false
+        }
+        
+    } else{
+        return "Algum parâmetro não é uma string"
+    }
+}
+console.log(stringsEndsWith("Carro","ror"))
 
